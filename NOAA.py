abc9d087cf45f5ebcb64b0c6d2c9e796396ef6dc
Nicholas Leiker <leikernr@gmail.com>
2025-10-27 10:45:11 -0500

    Update NOAA.py

--- NOAA.py
+++ NOAA.py
@@ -84,7 +84,8 @@ folium.CircleMarker(
 st_folium(m, width=700, height=400)
 
 st.success("""
-**This is how I processed sonar at 600 ft below sea.**  
-**Now I’ll do it for your rig at 6,000 ft.**  
+**This is how I processed sonar at 5000 ft below sea.**  
+**Now I’ll do it for your rig at 55,000 ft.**  
 [Contact Me on LinkedIn](www.linkedin.com/in/nicholas-leiker-50686755) | MRE Consulting Ready
+
 """)
